0398816008867bec7ebbc4327fc5415ff56e06f7
minibot47 <toludairo534@gmail.com>
2024-05-30 23:57:46 +0100

    started working on setting

--- my-react-project/src/pages/Settings.js
+++ my-react-project/src/pages/Settings.js
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import './Settings.css'
+import not from '../images/not.png';
+import profile from '../images/profile.png';
 
 const Settings = () => {
   return (
-    <div>Settings</div>
+    <div className='Settings'>
+      <div className='innerrightAppointmentstop'>
+        <div className='Appointmentsstopleft'>
+          <h2 className='Appointmentspage'>Home {'>'} <span className='blueAppointments'> Setting</span></h2>
+          <div className='Appointmentshealthtop'>
+            <h1 className='Appointmentspagetop'>Setting</h1>
+            <div className='Appointmentshealth'>
+            </div>
+          </div>
+        </div>
+        <div className='Appointmentstopright'>
+          <img src={not} alt="notifications"/>
+          <img src={profile} alt="profile"/>
+        </div>
+      </div>
+
+
+    </div>
   )
 }
 
